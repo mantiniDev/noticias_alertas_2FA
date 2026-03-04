7f38c77bf9943df78464b88fae363436b0b02dad
mantiniDev <mantinidev@gmail.com>
2026-03-04 08:28:48 -0300

    atualizado_urls

--- script.py
+++ script.py
@@ -52,40 +52,40 @@ TRIBUNAIS = [
     {"id": "126", "name": "Tribunal de Justiça de Roraima", "acronym": "TJRR", "url": "https://projudi.tjrr.jus.br/projudi/indisponibilidades.jsp"},
     {"id": "127", "name": "Tribunal de Justiça de Santa Catarina", "acronym": "TJSC", "url": "https://eproc1g.tjsc.jus.br/eproc/externo_controlador.php?acao=processo_disponibilidade_listagem"},
     {"id": "128", "name": "Tribunal de Justiça de São Paulo", "acronym": "TJSP", "url": "https://www.tjsp.jus.br/Indisponibilidade/Comunicados"},
-    {"id": "129", "name": "Tribunal de Justiça de Sergipe", "acronym": "TJSE", "url": "https://www.tjse.jus.br/pje/indisponibilidade-do-pje"},
-    {"id": "130", "name": "Tribunal de Justiça do Tocantins", "acronym": "TJTO", "url": "https://eproc1g.tjto.jus.br/eprocv2_prod_1g/externo_controlador.php?acao=processo_disponibilidade_listagem"},
+    {"id": "129", "name": "Tribunal de Justiça de Sergipe", "acronym": "TJSE", "url": "https://www.tjse.jus.br/portal/consultas/novo-cpc/indisponibilidade-de-sistemas"},
+    {"id": "130", "name": "Tribunal de Justiça do Tocantins", "acronym": "TJTO", "url": "https://www.tjto.jus.br/comunicacao/noticias"},
 
     # --- TRIBUNAIS REGIONAIS DO TRABALHO (24 TRTs) ---
-    {"id": "201", "name": "TRT 1ª Região (RJ)", "acronym": "TRT1", "url": "https://www.trt1.jus.br/certidao-de-indisponibilidade"},
-    {"id": "202", "name": "TRT 2ª Região (SP)", "acronym": "TRT2", "url": "https://ww2.trt2.jus.br/servicos/pje-processo-judicial-eletronico/indisponibilidade-do-pje/"},
-    {"id": "203", "name": "TRT 3ª Região (MG)", "acronym": "TRT3", "url": "https://portal.trt3.jus.br/internet/servicos/pje/indisponibilidade-do-sistema"},
+    {"id": "201", "name": "TRT 1ª Região (RJ)", "acronym": "TRT1", "url": "https://trt1.jus.br/certidao-de-indisponibilidade"},
+    {"id": "202", "name": "TRT 2ª Região (SP)", "acronym": "TRT2", "url": "https://aplicacoes8.trt2.jus.br/sis/indisponibilidade/consulta"},
+    {"id": "203", "name": "TRT 3ª Região (MG)", "acronym": "TRT3", "url": "https://cinde.trt3.jus.br/cinde/certidao/listagem.htm?dswid=-6858"},
     {"id": "204", "name": "TRT 4ª Região (RS)", "acronym": "TRT4", "url": "https://www.trt4.jus.br/portais/trt4/pje-indisponibilidade"},
     {"id": "205", "name": "TRT 5ª Região (BA)", "acronym": "TRT5", "url": "https://portalpje.trt5.jus.br/pje-indisponibilidades"},
-    {"id": "206", "name": "TRT 6ª Região (PE)", "acronym": "TRT6", "url": "https://www.trt6.jus.br/portal/pje/indisponibilidade-do-sistema"},
-    {"id": "207", "name": "TRT 7ª Região (CE)", "acronym": "TRT7", "url": "https://www.trt7.jus.br/pje/indisponibilidade-do-sistema"},
+    {"id": "206", "name": "TRT 6ª Região (PE)", "acronym": "TRT6", "url": "https://www.trt6.jus.br/portal/noticias"},
+    {"id": "207", "name": "TRT 7ª Região (CE)", "acronym": "TRT7", "url": "https://www.trt7.jus.br/index.php/blog/200-servicos/227-pje/7512-indisponibilidades-do-pje?start=1"},
     {"id": "208", "name": "TRT 8ª Região (PA/AP)", "acronym": "TRT8", "url": "https://www.trt8.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "209", "name": "TRT 9ª Região (PR)", "acronym": "TRT9", "url": "https://www.trt9.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "210", "name": "TRT 10ª Região (DF/TO)", "acronym": "TRT10", "url": "https://www.trt10.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "211", "name": "TRT 11ª Região (AM/RR)", "acronym": "TRT11", "url": "https://www.trt11.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "212", "name": "TRT 12ª Região (SC)", "acronym": "TRT12", "url": "https://trt12.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "213", "name": "TRT 13ª Região (PB)", "acronym": "TRT13", "url": "https://www.trt13.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "214", "name": "TRT 14ª Região (RO/AC)", "acronym": "TRT14", "url": "https://trt14.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "215", "name": "TRT 15ª Região (Campinas)", "acronym": "TRT15", "url": "https://trt15.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "216", "name": "TRT 16ª Região (MA)", "acronym": "TRT16", "url": "https://www.trt16.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "217", "name": "TRT 17ª Região (ES)", "acronym": "TRT17", "url": "https://www.trt17.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "218", "name": "TRT 18ª Região (GO)", "acronym": "TRT18", "url": "https://www.trt18.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "219", "name": "TRT 19ª Região (AL)", "acronym": "TRT19", "url": "https://www.trt19.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "220", "name": "TRT 20ª Região (SE)", "acronym": "TRT20", "url": "https://www.trt20.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "221", "name": "TRT 21ª Região (RN)", "acronym": "TRT21", "url": "https://www.trt21.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "222", "name": "TRT 22ª Região (PI)", "acronym": "TRT22", "url": "https://www.trt22.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "223", "name": "TRT 23ª Região (MT)", "acronym": "TRT23", "url": "https://portal.trt23.jus.br/pje/indisponibilidade-do-sistema"},
-    {"id": "224", "name": "TRT 24ª Região (MS)", "acronym": "TRT24", "url": "https://www.trt24.jus.br/pje/indisponibilidade-do-sistema"},
+    {"id": "209", "name": "TRT 9ª Região (PR)", "acronym": "TRT9", "url": "https://www.trt9.jus.br/portal/destaques.xhtml"},
+    {"id": "210", "name": "TRT 10ª Região (DF/TO)", "acronym": "TRT10", "url": "https://www.trt10.jus.br/servicos/?pagina=pje/indisponibilidade/index.php"},
+    {"id": "211", "name": "TRT 11ª Região (AM/RR)", "acronym": "TRT11", "url": "https://portal.trt11.jus.br/index.php/advogados/pagina-pje/33-pje/365-periodos-de-indisponibilidade-do-pje-jt"},
+    {"id": "212", "name": "TRT 12ª Região (SC)", "acronym": "TRT12", "url": "https://portal.trt12.jus.br/pje/uso_indisponibilidade"},
+    {"id": "213", "name": "TRT 13ª Região (PB)", "acronym": "TRT13", "url": "https://www.trt13.jus.br/pje/relatorio-de-indisponibilidade-do-pje"},
+    {"id": "214", "name": "TRT 14ª Região (RO/AC)", "acronym": "TRT14", "url": "https://portal.trt14.jus.br/portal/pje/indisponibilidade"},
+    {"id": "215", "name": "TRT 15ª Região (Campinas)", "acronym": "TRT15", "url": "https://trt15.jus.br/pje/indisponibilidade-pje"},
+    {"id": "216", "name": "TRT 16ª Região (MA)", "acronym": "TRT16", "url": "https://www.trt16.jus.br/servicos/outros-servicos/calendario-indisponibilidade"},
+    {"id": "217", "name": "TRT 17ª Região (ES)", "acronym": "TRT17", "url": "https://www.trt17.jus.br/web/servicos/w/indiponibilidade-sistema-pje"},
+    {"id": "218", "name": "TRT 18ª Região (GO)", "acronym": "TRT18", "url": "https://www.trt18.jus.br/portal/servicos/pje/indisponibilidades-do-pje/"},
+    {"id": "219", "name": "TRT 19ª Região (AL)", "acronym": "TRT19", "url": "https://site.trt19.jus.br/pjePeridosIndisponibilidades"},
+    {"id": "220", "name": "TRT 20ª Região (SE)", "acronym": "TRT20", "url": "https://www.trt20.jus.br/pje/indisponibilidade"},
+    {"id": "221", "name": "TRT 21ª Região (RN)", "acronym": "TRT21", "url": "https://www.trt21.jus.br/servicos/pje/indisponibilidade-sistema"},
+    {"id": "222", "name": "TRT 22ª Região (PI)", "acronym": "TRT22", "url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vT1ihmu1f1FyY-MGX2fo5ikUpRwf2QVq7KSZSANyf-r0deXMtoMM2L-pw_kFR6nFXXrdd4mdrFNKJ3F/pubhtml?gid=0&single=true"},
+    {"id": "223", "name": "TRT 23ª Região (MT)", "acronym": "TRT23", "url": "https://portal.trt23.jus.br/portal/calendario-de-indisponibilidade-do-pje"},
+    {"id": "224", "name": "TRT 24ª Região (MS)", "acronym": "TRT24", "url": "https://www.trt24.jus.br/indisponibilidade-do-pje"},
 
     # --- TRIBUNAIS REGIONAIS ELEITORAIS (TREs selecionados) ---
     {"id": "300", "name": "TSE (Nacional)", "acronym": "TSE", "url": "https://www.tse.jus.br/servicos-judiciais/processos/processo-judicial-eletronico/situacao-atual-dos-servicos-digitais-do-tse"},
     {"id": "301", "name": "TRE São Paulo", "acronym": "TRE-SP", "url": "https://www.tre-sp.jus.br/servicos-judiciais/indisponibilidade-pje"},
     {"id": "302", "name": "TRE Minas Gerais", "acronym": "TRE-MG", "url": "https://www.tse.jus.br/servicos-judiciais/processos/processo-judicial-eletronico/situacao-atual-dos-servicos-digitais-do-tse"},
-    {"id": "303", "name": "TRE Rio de Janeiro", "acronym": "TRE-RJ", "url": "https://www.tre-rj.jus.br/servicos-judiciais/pje/indisponibilidade-do-sistema-pje"}
+    {"id": "303", "name": "TRE Rio de Janeiro", "acronym": "TRE-RJ", "url": "https://www.tre-rj.jus.br/servicos-judiciais/comunicados/comunicados"}
 ]
 
 TERMOS_ESPECIFICOS = [
@@ -106,7 +106,7 @@ TERMOS_ESPECIFICOS = [
 
 TERMOS_FORTES_TI = [
     "pje", "eproc", "projudi", "esaj", "pdpj", "jus.br", 
-    "2fa", "mfa", "duplo fator", "dois fatores", "multifator", "authenticator", 
+    "2fa", "2FA", "mfa", "duplo fator", "dois fatores", "multifator", "authenticator", 
     "sso", "single sign-on", "captcha", "waf", "token", "ciberataque", 
     "instabilidade", "indisponibilidade tribunais", "golpe do advogado",
     "portaria nº 140", "resolução nº 335", "certificado digital",
@@ -208,7 +208,7 @@ def gerar_corpos_email(noticias):
     uma_semana_atras = (datetime.now() - timedelta(days=7)).strftime("%d/%m/%Y")
     
     # 1. Versão Texto Puro
-    texto_puro = f"MANST - Monitoramento Automatizado de Notícias de Sistemas e Tribunais ({uma_semana_atras} a {hoje})\n\n"
+    texto_puro = f"MAST - Monitoramento Automatizadode Sistemas e Tribunais ({uma_semana_atras} a {hoje})\n\n"
     
     # 2. Versão HTML
     html = f"""
@@ -244,11 +244,11 @@ def gerar_corpos_email(noticias):
             """
         html += "</ul>"
 
-    rodape = "\n---\nEste é um e-mail automático gerado pelo monitoramento automatizado de notícias de sistemas e tribunais."
+    rodape = "\n---\nEste é um e-mail automático gerado pelo MAST."
     texto_puro += rodape
     html += f"""
         <hr style="border: 1px solid #eee;">
-        <p style="font-size: 0.8em; color: #95a5a6;">Este é um e-mail automático gerado pelo monitoramento automatizado de notícias de sistemas e tribunais.</p>
+        <p style="font-size: 0.8em; color: #95a5a6;">Este é um e-mail automático gerado pelo MAST.</p>
       </body>
     </html>
     """
